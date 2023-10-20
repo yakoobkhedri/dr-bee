a9ad627a0fcb27bc4b15a8d31ed0723be6e5d255
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-10-20 22:49:14 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,8 +4,24 @@ AOS.init();
 // ham menu
 
 let hamIcon=document.getElementById('hamIcon');
+let closeIcon=document.getElementById('closeIcon');
 let menu=document.querySelector('header ul');
 
 hamIcon.addEventListener('click',function () {
-    menu.classList.toggle('active');
+    menu.classList.add('active');
+    overlay.classList.add('active');
+    closeIcon.style.display='block';
+    hamIcon.style.display='none';
+})
+closeIcon.addEventListener('click',function () {
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+    closeIcon.style.display='none';
+    hamIcon.style.display='block';
+})
+overlay.addEventListener('click',function () {
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+    closeIcon.style.display='none';
+    hamIcon.style.display='block';
 })
